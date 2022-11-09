b2d441956d7f7ce95ac3bf8d4a0f09afc06b5ac0
motive441 <motive441@gmail.com>
2022-11-10 00:01:35 +0600

    finished my database

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    roll = models.CharField(max_length=200)
+
+
+    def __str__(self):
+        return self.student_name
+
+
+class Information(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.student.student_name
